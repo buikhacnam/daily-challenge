cf327c0f86abdea4303514284c5de7c982da0d05
Bui Khac Nam <67185512+buikhacnam@users.noreply.github.com>
2020-10-29 16:06:30 +0700

    Add files via upload

---
+++ longestCommonPrefix.js
@@ -0,0 +1,23 @@
+var longestCommonPrefix = function(strs) {
+  let prefix = '';
+  if (strs.length === 0 || strs === null ) {
+    return prefix;
+  }
+
+  for (let i = 0; i < strs[0].length; i++) {
+      let char = strs[0][i];
+
+      for (let j = 1; j < strs.length; j++) {
+        if (strs[j][i] !== char) {
+          return prefix;
+        }
+      } prefix = prefix + char;
+  }
+
+  return prefix;
+    
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"])) // "fl"
+
+
